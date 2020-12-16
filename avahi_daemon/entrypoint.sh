85810e453d2c0dd83156b9713d364234d32feb5b
KavithaVarughese <kavithavarughese@gmail.com>
2020-12-17 00:53:25 +0530

    docker-compose with avahi and node.js backend and sql. Avahi can read from sql

--- avahi_daemon/entrypoint.sh
+++ avahi_daemon/entrypoint.sh
@@ -3,8 +3,8 @@
 set -e
 service dbus start
 service avahi-daemon start
-sleep 5s
-avahi-publish -s rasspb170_1-airplay-srv-host _airplay._tcp 1 rasspb170_1-airplay-txt-host
+#bash avahi/start.sh
+tail -F /dev/null
 exec "$@"
 
 
---
+++ db/sql-scripts/InsertData.sql
@@ -0,0 +1,3 @@
+INSERT INTO announce (serv_name, serv_type, `desc`, ipv4, ipv6, host, mac, ttl, hash)
+VALUES
+    ('kavitha', '_airplay._tcp','kavitha-txt', '10.0.2.15', 'fe80::99c9:7ece:7817:44b5','host', '0a:0b:0c:0d:0e:0f', 4500, 'sdkjahfiuwhfkajsdfbasdfwerq');
